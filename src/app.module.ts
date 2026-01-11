f4f25b0aba897f9500f1087fe4dbdda7ab092e64
Carlos Galindo <utm22030587@utma.edu.mx>
2026-01-11 13:06:36 -0600

    feat: load and validate environment variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { CommonModule } from './common/common.module';
+import { envConfiguration } from './common/config/env.config';
+import { ZodEnvSchema } from './common/config/env.validation';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [envConfiguration],
+      validate: (config) => {
+        return ZodEnvSchema.parse(config);
+      },
+    }),
+    CommonModule,
+  ],
   controllers: [],
   providers: [],
 })
