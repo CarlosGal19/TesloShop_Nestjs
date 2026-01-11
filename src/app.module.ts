cdbd60cea7d6b15bb4e10ce17746a35810ba9805
Carlos Galindo <utm22030587@utma.edu.mx>
2026-01-11 13:17:45 -0600

    feat: create db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,29 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { CommonModule } from './common/common.module';
-import { envConfiguration } from './common/config/env.config';
+// import { envConfiguration } from './common/config/env.config';
 import { ZodEnvSchema } from './common/config/env.validation';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
-      load: [envConfiguration],
+      // load: [envConfiguration],
       validate: (config) => {
         return ZodEnvSchema.parse(config);
       },
     }),
     CommonModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT || ''),
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      entities: [],
+      synchronize: true,
+    }),
   ],
   controllers: [],
   providers: [],
