9e62ca1dac9661e6e876020a7f0f9d8f4df32bae
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-01 14:05:07 -0600

    feat: implement authorization in seed endpoint

--- src/seed/seed.module.ts
+++ src/seed/seed.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { SeedController } from './seed.controller';
 import { SeedService } from './seed.service';
 import { ProductsModule } from '../products/products.module';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   controllers: [SeedController],
   providers: [SeedService],
-  imports: [ProductsModule],
+  imports: [ProductsModule, AuthModule],
 })
 export class SeedModule {}
