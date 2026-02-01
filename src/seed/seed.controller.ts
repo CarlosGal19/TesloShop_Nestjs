9b50cfeb705b2620aa3c6d8eced827a06dd03533
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-01 14:40:22 -0600

    feat: save user id when seed is executed

--- src/seed/seed.controller.ts
+++ src/seed/seed.controller.ts
@@ -2,6 +2,8 @@ import { Controller, Get } from '@nestjs/common';
 import { SeedService } from './seed.service';
 import { Auth } from '../auth/decorators/auth.decorator';
 import { ValidRoles } from '../auth/interfaces/valid-roles.interface';
+import { GetUser } from '../auth/decorators/get-user.decorator';
+import { User } from '../auth/entities/user.entity';
 
 @Controller('seed')
 export class SeedController {
@@ -9,7 +11,7 @@ export class SeedController {
 
   @Get()
   @Auth(ValidRoles.admin)
-  runSeed() {
-    return this.seedService.runSeed();
+  runSeed(@GetUser() user: User) {
+    return this.seedService.runSeed(user);
   }
 }
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -5,13 +5,14 @@ import {
 } from '@nestjs/common';
 import { ProductsService } from '../products/products.service';
 import { responseConfig } from '../common/global/response.config';
+import { User } from '../auth/entities/user.entity';
 import { initialData } from './data/seed';
 
 @Injectable()
 export class SeedService {
   private readonly logger = new Logger('SeedService');
   constructor(private readonly productsService: ProductsService) {}
-  async runSeed() {
+  async runSeed(user: User) {
     try {
       await this.productsService.deleteAllProducts();
 
@@ -20,7 +21,7 @@ export class SeedService {
       const insertPromises: any[] = [];
 
       SEED_PRODUCTS.forEach((product) => {
-        insertPromises.push(this.productsService.create(product));
+        insertPromises.push(this.productsService.create(product, user));
       });
 
       await Promise.all(insertPromises);
