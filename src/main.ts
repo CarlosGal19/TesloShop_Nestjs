efa95e8452a1c13f0dcc07b25f0b010c000b2a0b
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-01 15:57:54 -0600

    chore: set up initial swagger configuration

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -15,6 +16,18 @@ async function bootstrap() {
     }),
   );
   app.setGlobalPrefix('api/v1');
+
+  if (process.env.NODE_ENV === 'dev') {
+    const config = new DocumentBuilder()
+      .setTitle('Teslo Shop Docs')
+      .setDescription('Swagger generation for Teslo Shop API')
+      .setVersion('1.0')
+      .build();
+
+    const documentFactory = () => SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('docs', app, documentFactory);
+  }
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
