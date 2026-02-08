0b05af8cb7da78d85dfedcd476a1f0ac65dbbd4f
Carlos Galindo <c.galindo.islas@gmail.com>
2026-02-07 19:33:12 -0600

    feat: import auth module

--- src/ws_gateway/ws_gateway.module.ts
+++ src/ws_gateway/ws_gateway.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
 import { WsGatewayService } from './ws_gateway.service';
 import { WsGatewayGateway } from './ws_gateway.gateway';
+import { AuthModule } from '../auth/auth.module';
 
 @Module({
   providers: [WsGatewayGateway, WsGatewayService],
+  imports: [AuthModule],
 })
 export class WsGatewayModule {}
